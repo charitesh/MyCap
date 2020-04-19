ce583cef98e233e337dcc77c8d17ec2ab1e827ec
charitesh <63202266+charitesh@users.noreply.github.com>
2020-04-19 20:28:57 +0530

    Created Tupaccess.py
    This file is used to access elements from tuple

---
+++ Tupaccess.py
@@ -0,0 +1,20 @@
+#Accessing elements from tuple
+
+#creating tuple
+tup = (1, 2, "hi", "hello")
+
+#accessing all elements of tup
+print("Displaying all elements in tup: ", tup)
+
+#accessing 1st element
+print("1st element in tup =", tup[0])
+
+#accessing 3rd element
+print("last element in tup =", tup[3])
+
+#using negative index to access tuple
+#accessing elements
+print(tup[-4])
+print(tup[-3])
+print(tup[-2])
+print(tup[-1])
