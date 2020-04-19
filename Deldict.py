716461f80499cc714f115dc5c4b09cc4541feed5
charitesh <63202266+charitesh@users.noreply.github.com>
2020-04-19 20:30:38 +0530

    Created Deldict.py
    This file deletes different elements from dictionary

---
+++ Deldict.py
@@ -0,0 +1,11 @@
+#Deleting different dictionary elements
+
+#creating dictionary
+dictdel = {1: 'hi', 2: 'hello', 3:'bye'}
+print("Elements in dictionary before deleting: ", dictdel)
+#removing 1st element
+del dictdel[1]
+print("Dictionary after removing 1st element: ", dictdel)
+#removing 3rd element
+del dictdel[3]
+print("Dictionary after removing 3rd element: ", dictdel)
